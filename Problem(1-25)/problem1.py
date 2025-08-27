b8120f273f318d3c6d6878701981e8acb234a697
Aononto Jahan Junnurain <aonontojahan@gmail.com>
2025-08-28 05:10:52 +0600

    Files Formating Updated

--- Problem(1-25)/problem1.py
+++
@@ -1,9 +0,0 @@
-#A program for adding two number
-
-num1=int(input("Please enter first integer number : "))
-num2=int(input("Please enter secound integer number : "))
-
-sum=num1+num2
-
-print("First Number: {}, Second Number: {}".format(num1,num2))
-print("The summation of the two number is : ",sum)
